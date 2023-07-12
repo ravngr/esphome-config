b25acf73d253688fc455e0103c0970a428331d48
Chris Harrison <629204+ravngr@users.noreply.github.com>
2023-07-12 11:44:01 +0000

    Cleanup CI secrets Fixed test script

--- script/test_esphome.sh
+++ script/test_esphome.sh
@@ -3,41 +3,28 @@
 ## Globals/imports
 SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export SCRIPT_PATH
-
 REPO_PATH=$(readlink -f "${SCRIPT_PATH}/..")
 export REPO_PATH
-
-. "${REPO_PATH}/deps/bashplate/bashplate.sh"
 
 
 ## CLI
 if [ -z "$1" ]; then
-    print_critical "Missing path argument"
+    echo "Missing path argument"
     exit 1
 fi
 
 if ! [[ "$1" =~ config/[^/]+.yaml ]]; then
-    print_info "Ignore $1: not a device YAML file"
+    echo "Ignore $1: not a device YAML file"
     exit 0
 fi
 
-
-## Test configuration
-file=$(basename "$1")
-
-if [[ -f "${REPO_PATH}/config/secrets.yaml" ]]; then
-    docker run \
-        --rm \
-        --name "esphome_config" \
-        -v "${REPO_PATH}/config:/config" \
-        "ghcr.io/esphome/esphome:${ESPHOME_TAG:-stable}" \
-        config "${file}"
-else
-    docker run \
-        --rm \
-        --name "esphome_config" \
-        -v "${REPO_PATH}/config:/config" \
-        -v "${REPO_PATH}/config/secrets.ci.yaml:/config/secrets.yaml" \
-        "ghcr.io/esphome/esphome:${ESPHOME_TAG:-stable}" \
-        config "${file}"
+if [[ "$1" =~ config/secrets[^/]+.yaml ]]; then
+    echo "Ignore $1: secret YAML file"
+    exit 0
 fi
+
+if ! [[ -f "${REPO_PATH}/config/secrets.yaml" ]]; then
+    ln -s "${REPO_PATH}/config/secrets.ci.yaml" "${REPO_PATH}/config/secrets.yaml"
+fi
+
+esphome config "$1"
